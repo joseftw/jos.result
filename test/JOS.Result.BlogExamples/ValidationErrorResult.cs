d647c6b0192f9149dc3cc6d2e5207bbd059c81df
joseftw <joseftw@localhost.local>
2021-02-17 21:52:59 +0100

    Adds some more example results

---
+++ test/JOS.Result.BlogExamples/ValidationErrorResult.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace JOS.Result.BlogExamples
+{
+    public class ValidationErrorResult : ErrorResult
+    {
+        public ValidationErrorResult(string message) : base(message)
+        {
+        }
+
+        public ValidationErrorResult(string message, IReadOnlyCollection<ValidationError> errors) : base(message, errors)
+        {
+        }
+    }
+
+    public class ValidationError : Error
+    {
+        public ValidationError(string propertyName, string details) : base(null, details)
+        {
+            PropertyName = propertyName;
+        }
+
+        public string PropertyName { get; }
+    }
+}

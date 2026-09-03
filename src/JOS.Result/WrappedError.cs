7e57e44adf297ce3badffd559cc04b52f6adee9a
joseftw <1232096+joseftw@users.noreply.github.com>
2026-09-03 11:53:00 +0200

    Adds WrappedError

---
+++ src/JOS.Result/WrappedError.cs
@@ -0,0 +1,15 @@
+﻿namespace JOSResult;
+
+/// <summary>
+/// Base class for errors that wrap another <see cref="Error"/> while presenting a different message.
+/// </summary>
+
+public abstract class WrappedError : Error
+{
+    protected WrappedError(string message, Error error) : base(error.ErrorType, message)
+    {
+        Error = error;
+    }
+
+    public Error Error { get; }
+}
---
+++ test/JOS.Result.Tests/WrappedErrorTests.cs
@@ -0,0 +1,27 @@
+﻿using JOSResult;
+using Shouldly;
+using Xunit;
+
+namespace JOS.Result.Tests;
+
+public class WrappedErrorTests
+{
+    [Fact]
+    public void ShouldReturnPublicErrorWhenReadingErrorMessage()
+    {
+        var notFoundError = new NotFoundError("Account", "some-id");
+
+        var result = new PublicError("This is the public message", notFoundError);
+
+        result.ErrorMessage.ShouldBe("This is the public message");
+        result.ErrorType.ShouldBe(notFoundError.ErrorType);
+        result.Error.ShouldBe(notFoundError);
+    }
+}
+
+public class PublicError : WrappedError
+{
+    public PublicError(string message, Error error) : base(message, error)
+    {
+    }
+}
